d5d95de6b8c3932e31ced90c9be323d2119baf05
Christopher A. Llones <christopher.llones@vsu.edu.ph>
2026-03-19 00:16:55 +0800

    housing materials

--- vsu-housing-inventory/data-management.R
+++ vsu-housing-inventory/data-management.R
@@ -4,9 +4,11 @@ custom_theme <- function() {
   theme(plot.margin = margin(20, 20, 20, 20),
         plot.title = element_text(size = 16, face = "bold", margin = margin(b = 10)),
         plot.subtitle = element_text(size = 14, hjust = 0),
-        axis.text = element_text(size = 12),
+        axis.text.x = element_text(size = 12, margin = margin(b=10)),
+        axis.text.y = element_text(size = 12),
         axis.title.x = element_text(size = 16, margin = margin(t = 10)),
         axis.title.y = element_text(size = 16, margin = margin(r = 10)),
+        strip.text = element_text(size = 14),
         legend.position = "bottom"
   )
 }
